13ae0cd2a67640468581b0959f6ff5ac7b7ec4f7
HaRsHGoRR <heet.s.gor@gmail.com>
2024-02-22 01:23:39 +0530

    error responsive

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Flex, Heading, Text, Image } from '@chakra-ui/react';
 
 const Error = () => {
-  // Add event listener to disable scrolling when component mounts
   React.useEffect(() => {
     document.body.style.overflow = 'hidden';
-    // Cleanup function to re-enable scrolling when component unmounts
     return () => {
       document.body.style.overflow = 'auto';
     };
@@ -18,12 +16,18 @@ const Error = () => {
       h="100vh"
       bg="blue.950"
       direction="column"
+      px={{ base: '4', md: '8', lg: '16', xl: '32' }} // Adjust padding for different screen sizes
     >
-      <Image src="https://media1.tenor.com/m/FcVg5W9zZJQAAAAC/error.gif" alt="404" />
-      <Heading as="h1" size="xl" mt="4" color="blue.700">
+      <Image
+        src="https://media1.tenor.com/m/FcVg5W9zZJQAAAAC/error.gif"
+        alt="404"
+        maxW={{ base: '80%', md: '60%', lg: '40%', xl: '30%' }} // Adjust image width for different screen sizes
+        mb={{ base: '8', md: '12' }} // Adjust margin bottom for different screen sizes
+      />
+      <Heading as="h1" size="xl" textAlign="center" color="blue.700" mb="4">
         Sorry, this page isn't available
       </Heading>
-      <Text fontSize="lg" mt="2" color="blue.600">
+      <Text fontSize="lg" textAlign="center" color="blue.600">
         The link you followed may be broken, or the page may have been removed.
         Go back to the home page.
       </Text>
