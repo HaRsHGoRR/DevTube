12315c7aaffe60638415a4d220cc7d22e7b118d8
abhay14gohel <abhaygohel777@gmail.com>
2024-03-30 04:49:10 +0530

    FINAL: DEPLOYED FRONTEND.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    proxy: {
-      "/api": {
-       target: "http://localhost:5000",
-        changeOrigin: true,
-
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  base: "/"
+ 
 });
+
+
+// due to deployment removed this code, this is for devlopment.
+//  server: {
+//     proxy: {
+//       "/api": {
+//        target: "http://localhost:5000",
+//         changeOrigin: true,
+
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+
 
 // rewrite: (path) => path.replace(/^\/api/, ""),
 //  secure: false,
