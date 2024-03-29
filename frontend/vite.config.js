4d7c30b01b5e79666eb201f880244c33dd851cdb
abhay14gohel <abhaygohel777@gmail.com>
2024-03-29 22:47:28 +0530

    deploying frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://devtube-zrqq.onrender.com/",
         changeOrigin: true,
 
         rewrite: (path) => path.replace(/^\/api/, ""),
